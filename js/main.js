f20195a2ef319d138de571ab8008e231717b6f86
j-ohene <jesicaohene1@gmail.com>
2023-07-25 10:20:23 -0400

    draggable elements to gifs

--- js/main.js
+++ js/main.js
@@ -1,12 +1,18 @@
 //Javascript
 const theSynth = document.querySelector("#theSynth");
+const boomBox = document.querySelector("#boomBox");
+const thePiano = document.querySelector("#thePiano");
+const theDrum = document.querySelector("#theDrum");
 const danceFloor = document.querySelector("#danceFloor");
 
-let draggedpiece;
-console.log(theSynth);
+let draggedPiece;
+console.log(theSynth),
+console.log(boomBox),
+console.log(thePiano),
+console.log(theDrum),
 console.log(danceFloor);
 
-//funtion
+//function
 function startedDragging (){
     console.log ("started dragging");
     draggedPiece = this;
@@ -22,6 +28,9 @@ function draggedOver (e) {
  }
  //eventListeners
 theSynth.addEventListener("dragstart", startedDragging);
+boomBox.addEventListener("dragstart", startedDragging);
+thePiano.addEventListener("dragstart", startedDragging);
+theDrum.addEventListener("dragstart", startedDragging);
 danceFloor.addEventListener("dragover", draggedOver);
 danceFloor.addEventListener("drop", dropped);
 
