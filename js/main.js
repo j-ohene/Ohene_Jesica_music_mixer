a36be53be89e6f82a06b5286a58036a9ba7fea81
j-ohene <jesicaohene1@gmail.com>
2023-07-25 13:17:07 -0400

    additional sounds

--- js/main.js
+++ js/main.js
@@ -4,6 +4,7 @@ const boomBox = document.querySelector("#boomBox");
 const thePiano = document.querySelector("#thePiano");
 const theDrum = document.querySelector("#theDrum");
 const danceFloor = document.querySelector("#danceFloor");
+const resetButton = document.querySelector("button");
 
 let draggedPiece;
 console.log(theSynth),
@@ -21,6 +22,11 @@ console.log(boomBox)
 let piano = document.createElement ("audio")
 console.log(thePiano)
 
+let amp = document.createElement ("audio")
+console.log(theDrum)
+
+
+
 //function
 function startedDragging (){
     console.log ("started dragging");
@@ -37,7 +43,7 @@ function draggedOver (e) {
     //lets makes some music//
 synth.src = "audio/synth-2.wav";
 synth.load();
-document.body.appendChild(synth);
+document.body.appendChild(synth)
 synth.play();
 
 boombox.src = "audio/Digi-Bass-03.wav";
@@ -45,10 +51,15 @@ boombox.load();
 document.body.appendChild(boombox);
 boombox.play();
 
-piano.src = "audio/House-Piano.wav";
+piano.src = "audio/House-Piano.mp3";
 piano.load();
 document.body.appendChild(piano);
 piano.play();
+
+amp.src = "audio/snare.wav";
+amp.load();
+document.body.appendChild(amp);
+amp.play();
 
  }
 
@@ -59,7 +70,5 @@ thePiano.addEventListener("dragstart", startedDragging);
 theDrum.addEventListener("dragstart", startedDragging);
 danceFloor.addEventListener("dragover", draggedOver);
 danceFloor.addEventListener("drop", dropped);
-synth.addEventListener("drop", dropped);
-boombox.addEventListener("drop", dropped);
-piano.addEventListener("drop", dropped);
+
 
