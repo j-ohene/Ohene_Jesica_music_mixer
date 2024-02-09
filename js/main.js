f2826cca819b6df247f78cdd753d8e8af59f7451
j-ohene <jesicaohene1@gmail.com>
2024-02-09 18:42:03 -0500

    associated game pieces with elements

--- js/main.js
+++ js/main.js
@@ -4,27 +4,12 @@ const boomBox = document.querySelector("#boomBox");
 const thePiano = document.querySelector("#thePiano");
 const theDrum = document.querySelector("#theDrum");
 const danceFloor = document.querySelector("#danceFloor");
-const returnButton = document.querySelector("#returnButton");
 const instruments = document.querySelector("#theBand");
 
 
 let originalParent = document.querySelector("#theBand");
 
 let draggedPiece;
-
-
-let synth = document.createElement ("audio")
-console.log(theSynth)
-
-let boombox = document.createElement ("audio")
-console.log(boomBox)
-
-let piano = document.createElement ("audio")
-console.log(thePiano)
-
-let amp = document.createElement ("audio")
-console.log(theDrum)
-
 
 
 //function
@@ -36,42 +21,29 @@ function draggedOver (e) {
     e.preventDefault();
     console.log ("Item was dragged over dropzone");
  }
- function dropped (e) {
-    e.preventDefault();
-    console.log("Item was dropped");
-    this.appendChild(draggedPiece);
-
-    function returnToOriginalPosition() {
-        if (draggedPiece ) {
-            originalParent.appendChild(draggedPiece);
-            draggedPiece = null;
-            originalParent = null;
-        }
-    }
-
-    //to play music// 
-    
-synth.src = "audio/synth-2.wav";
-synth.load();
-document.body.appendChild(synth);
-synth.play();
-
-boombox.src = "audio/Digi-Bass-03.wav";
-boombox.load();
-document.body.appendChild(boombox);
-boombox.play();
-
-piano.src = "audio/House-Piano.mp3";
-piano.load();
-document.body.appendChild(piano);
-piano.play();
-
-amp.src = "audio/snare.wav";
-amp.load();
-document.body.appendChild(amp);
-amp.play();
-
+ let synth = new Audio("audio/synth-2.wav");
+ let boombox = new Audio("audio/Digi-Bass-03.wav");
+ let piano = new Audio("audio/House-Piano.mp3");
+ let amp = new Audio("audio/snare.wav");
+ 
+ function dropped(e) {
+     e.preventDefault();
+     console.log("Item was dropped");
+     this.appendChild(draggedPiece);
+ 
+     
+     if (draggedPiece ==theSynth) {
+         synth.play();
+     } else if (draggedPiece == boomBox) {
+         boombox.play();
+     } else if (draggedPiece ==thePiano) {
+         piano.play();
+     } else if (draggedPiece == theDrum) {
+         amp.play();
+     }
+     
  }
+ 
 
  //eventListeners
 theSynth.addEventListener("dragstart", startedDragging);
@@ -80,8 +52,4 @@ thePiano.addEventListener("dragstart", startedDragging);
 theDrum.addEventListener("dragstart", startedDragging);
 danceFloor.addEventListener("dragover", draggedOver);
 danceFloor.addEventListener("drop", dropped);
-
-returnButton.addEventListener("click",returnToOriginalPosition);
-
-
 
